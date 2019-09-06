10970ad4b64e9618d04a960d916e3bda5601b36d
Oshada <oshadaamila.15@cse.mrt.ac.lk>
2019-09-06 11:21:29 +0530

    initial_commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tharaga';
+  list = ['first', 'seconf', 'third', 'fourth']
+  tharaga_list = ['ප්‍රායෝගික දැනුම', 'පද්‍ය ගායනා', 'දහම් දැනුම', 'ගද්‍ය රචනා', 'කථික', 'චිත්‍ර', 'මව් පිය ගුණ ගායනා']
+  grade_by_tharaga = { 'ප්‍රායෝගික දැනුම': ["1 ශ්‍ර‌ේණිය", "2 ශ්‍ර‌ේණිය"] }
+  grade_list = []
+  selected_value_tharaga_cmb = "තරගය තා‌ේරන්න‍‍‍‍..."
+  selected_value_grade_cmb = "ශ්‍ර‌ේණිය තා‌ේරන්න‍‍‍‍..."
+  gr_cmb_is_diabled = true
+
+  onSubmitClick() {
+    console.log("submit button clicked")
+  }
+  onGradeComboBoxItemSelect(item) {
+    console.log("grade combo box loaded"+item)
+  }
+
+  onTharagaComboBoxItemSelect(item) {
+    this.gr_cmb_is_diabled = false
+    this.selected_value_tharaga_cmb = item
+    this.grade_list = this.grade_by_tharaga[item]
+  }
 }
